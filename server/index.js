d532a97c7e1e35442804fa6bb84d1f1456723755
Andy Chen <chenyangpoppin@hotmail.com>
2020-10-29 22:22:43 +0800

    feat: add website source code

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const template = fs.readFileSync(path.join(__dirname, '../src/index.html'), 'utf-8');
+
+const server = (port) => {
+  const app = express();
+
+  // static target folder
+  app.use(express.static('src'));
+
+  // routers
+  app.get('/', (req, res) => {
+    const html = template;
+
+    res.status(200).send(html);
+  });
+
+  app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
+  });
+};
+
+server(process.env.PORT || 3000);
