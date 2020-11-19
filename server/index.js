9c0ea43da685a49e44fe20defa84ac29048ed2d2
Andy Chen <chenyangpoppin@hotmail.com>
2020-11-19 10:45:41 +0800

    feat: add cors & jsonp response support

--- server/index.js
+++ server/index.js
@@ -17,6 +17,32 @@ const server = (port) => {
     res.status(200).send(html);
   });
 
+  // jsonp test
+  app.get('/jsonp', (req, res) => {
+    const jsonpObj = {
+      name: 'jsonp',
+      data: [1, 2, 3, 4, 5, 6]
+    };
+
+    res.status(200).jsonp(jsonpObj);
+  });
+
+  // cors test
+  app.get('/cors', (req, res) => {
+    const retObj = {
+      name: 'cors',
+      data: [6, 5, 4, 3, 2, 1]
+    };
+
+    // allow cors at server: http://192.168.199.155:5000
+    res.header('Access-Control-Allow-Origin', 'http://192.168.199.155:5000');
+
+    // allow other server send cookie to this server
+    res.header('Access-Control-Allow-Credentials', true);
+
+    res.status(200).send(retObj);
+  });
+
   app.listen(port, () => {
     console.log('Server is running on port: ' + port);
   });
